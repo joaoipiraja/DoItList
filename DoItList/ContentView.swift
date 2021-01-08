bf28d6184f5b832a9fa07b65756f6fe74f996147
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-08 17:22:49 -0300

    Initial Commit

---
+++ DoItList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoItList
+//
+//  Created by João Victor Ipirajá de Alencar on 08/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DoItList/DoItListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoItListApp.swift
+//  DoItList
+//
+//  Created by João Victor Ipirajá de Alencar on 08/01/21.
+//
+
+import SwiftUI
+
+@main
+struct DoItListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
