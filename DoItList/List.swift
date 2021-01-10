0370e2ae3d64e1a873aaab4685f3e1adf6672064
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-10 17:33:36 -0300

    Update Data Model to Class ObservableObject

--- DoItList/List.swift
+++ DoItList/List.swift
@@ -7,18 +7,31 @@
 
 import SwiftUI
 
-struct Task: Identifiable{
-    var id:UUID
-    var name:String    
+class Task: Identifiable, ObservableObject{
+    @Published var id:UUID
+    @Published var name:String
+    
+    init(id:UUID, name:String){
+        self.id = id
+        self.name = name
+    }
 }
 
-struct Division: Identifiable{
+class Division: Identifiable, ObservableObject{
     
+    @Published var id: UUID
+    @Published var name: String
+    @Published var tasks: [Task]
+    @Published var color:Color
+    @Published var isDeleted:Bool
     
-    var id: UUID
-    var name: String
-    var tasks: [Task]
-    var color:Color
+    init(id:UUID, name:String,tasks: [Task], color:Color ){
+        self.id = id
+        self.name = name
+        self.tasks = tasks
+        self.color = color
+        self.isDeleted = false
+    }
     
     #if DEBUG
     static let example = Division(id: UUID(), name: "College", tasks: [Task(id: UUID(), name: "Study for Calculus Quiz")], color: Color(#colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)))
