ac9ecfe8a5d47c9726e2fcb89e3eb86c35f0ccd1
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-10 17:32:50 -0300

    Add remove functions

--- DoItList/ListManager.swift
+++ DoItList/ListManager.swift
@@ -10,9 +10,8 @@ import SwiftUI
 
 
 class ListManager: ObservableObject{
+    
     @Published var listDivision = [Division]()
-    
-    
     
     
     func addSection(name:String, color:Color){
@@ -22,22 +21,53 @@ class ListManager: ObservableObject{
             let sec_aux = Division(id: UUID(), name: name, tasks: tasks_aux, color: color)
             listDivision.append(sec_aux)
         
-        
     }
-    
+
    
     func addTask(at division:Division, with name:String){
         
         print(division.name)
-        if let index = listDivision.firstIndex(where: {$0.id == division.id}){
+        if let index = listDivision.firstIndex(where: {
+            
+           $0.id == division.id
+        }){
             let task_aux = Task(id: UUID(), name: name)
             print(index,task_aux)
             listDivision[index].tasks.append(task_aux)
+            
+        }
+        
+        
+        
+    }
+    
+    func removeDivision(at offsets: IndexSet){
+    
+        if let index = offsets.first{
+            listDivision[index].isDeleted = true
+            listDivision.remove(at: index)
+        }
+        
+        
+    }
+    
+    func removeTask(at offsets: IndexSet, division:Division){
+    
+        if let index_task = offsets.first{
+            
+            if let index_division = listDivision.firstIndex(where: {$0.id == division.id}){
+                listDivision[index_division].tasks.remove(at: index_task)
+            }
+            
         }
     }
     
-   
     
+    
+    
+
+    
+ 
     
     
 }
