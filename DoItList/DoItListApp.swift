5e1dd11e2302b90081fc4a70b178c489fda62225
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-11 16:46:18 -0300

    Hide keyboard when detect tap

--- DoItList/DoItListApp.swift
+++ DoItList/DoItListApp.swift
@@ -14,7 +14,7 @@ struct DoItListApp: App {
     var listManager = ListManager()
     var body: some Scene {
         WindowGroup {
-            ContentView().environmentObject(listManager)
+            ContentView().environmentObject(listManager).onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
         }
         .onChange(of: scenePhase){(newScenePhase) in
             switch newScenePhase{
@@ -30,5 +30,23 @@ struct DoItListApp: App {
                 print("App is in unknown state.")
             }
         }
+    }
+}
+
+
+extension UIApplication {
+    func addTapGestureRecognizer() {
+        guard let window = windows.first else { return }
+        let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapGesture.requiresExclusiveTouchType = false
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = self
+        window.addGestureRecognizer(tapGesture)
+    }
+}
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true // set to `false` if you don't want to detect tap during other gestures
     }
 }
